1eff343ce3b12a355f11644819170f0748534483
Marc Cavigli <mcavigli@gmail.com>
2019-08-20 05:14:05 +0000

    Added xargs

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,13 +11,13 @@ env.hosts = ['35.231.100.106', '35.237.151.115']
 def do_clean(number=0):
     """ deletes out of date archives """
     number = int(number)
-    with lcd('/versions'):
+    with lcd('versions'):
         if number == 0 or number == 1:
-            local('ls -ltr | head -n -1 | rm -rfv')
+            local('ls -tr | head -n -1 | xargs rm -rfv')
         else:
-            local('ls -ltr | head -n -{} | rm -rfv'.format(number))
-    with cd('data/web_static/releases'):
+            local('ls -tr | head -n -{} | xargs rm -rfv'.format(number))
+    with cd('/data/web_static/releases'):
         if number == 0 or number == 1:
-            run('ls -ltr | head -n - 1 | rm -rfv')
+            run('ls -tr | head -n - 1 | xargs rm -rfv')
         else:
-            run('ls -ltr | head -n -{} | rm -rfv'.format(number))
+            run('ls -tr | head -n -{} | xargs rm -rfv'.format(number))

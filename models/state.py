5791c5538152a720211f79af7dba1e5b4d3b98dc
Marc Cavigli <mcavigli@gmail.com>
2019-09-01 22:42:13 +0000

    Added conditionals to check for db or not

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, String
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -12,15 +13,18 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City",
-        cascade="all,delete,delete-orphan",
-        backref=backref("state", cascade="all,delete,delete-orphan"),
-        passive_deletes=True,
-        single_parent=True)
 
-    @property
-    def cities(self):
-        """returns list of City instances with state_id"""
-        return {k: v for k, v in storage.all().items()
-                if v.state_id == self.id}
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship(
+            "City",
+            cascade="all,delete,delete-orphan",
+            backref=backref("state", cascade="all,delete,delete-orphan"),
+            passive_deletes=True,
+            single_parent=True)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """returns list of City instances with state_id"""
+            return {k: v for k, v in storage.all().items()
+                    if v.state_id == self.id}
